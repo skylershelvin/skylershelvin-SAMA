4e5686191d2b0b2a832235e9a5914c8763c70b76
Skyler_Shelvin <skylershelvin@gmail.com>
2023-08-04 10:17:13 -0500

    allowed users to access certain pages without authentication. Tested to ensure authenticated pages are accessible

--- src/main/java/org/skylershelvin/SAMABackened/API/security/WebSecurityConfig.java
+++ src/main/java/org/skylershelvin/SAMABackened/API/security/WebSecurityConfig.java
@@ -5,15 +5,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 
 public class WebSecurityConfig {
 
+    private JwtRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable();
-                http.authorizeHttpRequests().anyRequest().permitAll();
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests()
+                // all pages I want to access without authentication
+                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                .anyRequest().authenticated();
         return http.build();
         }
     }
